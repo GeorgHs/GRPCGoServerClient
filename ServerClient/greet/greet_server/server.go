3011d8aa0df91b6344eba633dda86008ed0e5255
Georg Hertzsch <hertzsch@live.de>
2020-12-20 13:03:34 +0100

    Server Unary

--- ServerClient/greet/greet_server/server.go
+++ ServerClient/greet/greet_server/server.go
@@ -1,27 +1,36 @@
 package main
+
 import (
-    "fmt"
-    "log"
-    "net"
-	
-    
-    "google.golang.org/grpc"
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/simplesteph/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
 )
 
+type server struct{}
 
-
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello" + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
-    fmt.Println("Hello world")
-    
-    lis, err := net.Listen("tcp", "0.0.0.0:50051")
-    if err != nil {
-        log.Fatalf("Failed")
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed")
 	}
-	
+
 	s := grpc.NewServer()
 
-	
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
