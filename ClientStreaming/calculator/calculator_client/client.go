92a0e40c902c210ea284b6b4a7a91a9e467cc21c
Georg Hertzsch <hertzsch@live.de>
2020-12-22 12:42:39 +0100

    Compute Average

---
+++ ClientStreaming/calculator/calculator_client/client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/GeorgHs/GoServer/ClientStreaming/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I am a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+	//fmt.Println("Created client: %f",c)
+
+	//doUnary(c)
+	//c.Greet(context.Background(), in*greetpb.GreetRequest)
+	//doServerStreaming(c)
+	doClientStreaming(c)
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Printf("Starting to do a Unary RPC")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  5,
+		SecondNumber: 40,
+	}
+
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", res.SumResult)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Printf("Starting to do a PrimeDecomposition Server Streaming RPC")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 1239456756,
+	}
+
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling PrimeDecomposition RPC: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("format string", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
+}
+
+func doClientStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a ComputeAverage Client Streaming RPC...")
+
+	stream, err := c.ComputeAverage(context.Background())
+	if err != nil {
+		log.Fatalf("Error while opening stream %v", err)
+	}
+
+	numbers := []int32{3, 5, 9, 54, 23}
+
+	for _, number := range numbers {
+		fmt.Printf("Sending number: %v", number)
+		stream.Send(&calculatorpb.ComputeAverageRequest{
+			Number: number,
+		})
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving resposne: %v", err)
+	}
+
+	fmt.Println("The Average is: %v", res.GetAverage())
+}
---
+++ ClientStreaming/calculator/calculator_server/server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"github.com/GeorgHs/GoServer/ClientStreaming/calculator/calculatorpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
+	fmt.Printf("Received Sum RPC: %v", req)
+	firstNumber := req.FirstNumber
+	secondNumber := req.SecondNumber
+	sum := firstNumber + secondNumber
+	res := &calculatorpb.SumResponse{
+		SumResult: sum,
+	}
+	return res, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("Received PrimeNumberDecomposition RPC: %v\n", req)
+	number := req.GetNumber()
+	divisor := int64(2)
+
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			number = number / divisor
+		} else {
+			divisor++
+			fmt.Println("Divisor has increased to %v", divisor)
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Calculator Server")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed")
+	}
+
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Printf("Received ComputeAverage RPC\n")
+
+	sum := 0
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+		if err != nil {
+			log.Fatalf("error while reading client stream: %v", err)
+		}
+		sum += int(req.GetNumber())
+		count++
+	}
+}
